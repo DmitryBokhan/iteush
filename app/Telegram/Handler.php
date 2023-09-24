1c7ed51ef94b0631eaeb8576a72c1757bf4631e0
Dmitry <iteushm@gmail.com>
2023-09-24 12:24:09 +0300

    Добавлена обработка неизвестных команд

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -3,11 +3,27 @@
 namespace App\Telegram;
 
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use Illuminate\Support\Stringable;
 
 class Handler extends  WebhookHandler
 {
+
+    //простой ответ на команду
     public function hello(): void
     {
+        // это ответ бота на команду /hello
         $this->reply('Привет! Это твой первый бот в телеграм!');
+    }
+
+
+    //обрабатываем ответ бота на несуществующую команду
+    protected function handleUnknownCommand(Stringable $text): void
+    {
+        if ($text->value() == '/start') {
+            $this->reply('*Привет!* Давай начнем пользоваться мной! :-)');
+        } else {
+            $this->reply('*Неизвестная команда*');
+        }
+        
     }
 }
